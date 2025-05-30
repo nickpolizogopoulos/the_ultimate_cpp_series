e1ef11d8d9c026e59d24fd19b0142c269cb35648
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-05-30 12:49:03 +0300

    Add "Mac of two numbers" exercise.

---
+++ part-1-fundamentals/5-functions/1_max_of_two_numbers.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Nick Polizogopoulos on 30/5/25.
+//
+
+#include <iostream>
+
+// Create a function that receives two integers and returns the maximum of the two.
+
+int maxOfTwo(int first, int second) {
+    return first > second ? first : second;
+}
+
+void maxOfTwoNumbers() {
+
+    std::cout << "Exercise: Max of Two Numbers." << std::endl << "=======================================" << std::endl;
+
+    std::cout << "Enter Number #1: ";
+    int first;
+    std::cin >> first;
+
+    std::cout << "Enter Number #2: ";
+    int second;
+    std::cin >> second;
+
+    if (first == second)
+        std::cout << "Numbers are equal.";
+    else
+        std::cout << "The maximum number is: " << maxOfTwo(first, second) << std::endl;
+
+    std::cout << std::endl << std::endl;
+}
